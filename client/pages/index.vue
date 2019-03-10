3ac1d408476565b0af09b995846333988d96f6fb
unknown <bornasepic98@gmail.com>
2019-03-10 14:27:22 +0100

    Front end setup with Nuxt/Vuetify

---
+++ client/pages/index.vue
@@ -0,0 +1,18 @@
+<template lang="pug">
+  h1 Coin alert!!
+</template>
+
+<style lang="scss">
+  $black: red;
+
+  li {
+    color: $black
+  }
+  
+</style>
+
+<script>
+export default {
+  
+}
+</script>
